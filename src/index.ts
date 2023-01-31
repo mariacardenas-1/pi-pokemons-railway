ade940c1536a166ea0c1bc1c01e388a14eb808b8
mafe cardenas <maria.cardenas.r3@gmail.com>
2023-01-31 20:12:06 -0300

    ready to deployment

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,39 @@
-import bodyParser from "body-parser";
-import express from "express";
-import pg from "pg";
+// import bodyParser from "body-parser";
+// import express from "express";
+// import pg from "pg";
 
-// Connect to the database using the DATABASE_URL environment
-//   variable injected by Railway
-const pool = new pg.Pool();
+// // Connect to the database using the DATABASE_URL environment
+// //   variable injected by Railway
+// const pool = new pg.Pool();
 
-const app = express();
-const port = process.env.PORT || 3333;
+// const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+const server = require('./app.js');
+const { conn } = require('./db.js');
+
+const port = process.env.PORT || 3332;
+
+
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(port, '0.0.0.0')
+  server.listen(port, () => {
+    console.log(`%s listening at ${port} `); // eslint-disable-line no-console
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+// app.use(bodyParser.text({ type: "text/html" }));
+
+// app.get("/", async (req, res) => {
+//   const { rows } = await pool.query("SELECT NOW()");
+//   res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
