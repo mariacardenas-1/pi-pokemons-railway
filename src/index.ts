26acf60c009f709c542e39efc722a495e7e0f29b
mafe cardenas <maria.cardenas.r3@gmail.com>
2023-02-01 13:24:30 -0300

    prep for deploy

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-// import bodyParser from "body-parser";
-// import express from "express";
-// import pg from "pg";
 
-// // Connect to the database using the DATABASE_URL environment
-// //   variable injected by Railway
-// const pool = new pg.Pool();
-
-// const app = express();
 
 
 const server = require('./app.js');
@@ -17,23 +9,8 @@ const port = process.env.PORT || 3332;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(port, '0.0.0.0')
   server.listen(port, () => {
     console.log(`%s listening at ${port} `); // eslint-disable-line no-console
   });
 });
 
-
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-// app.use(bodyParser.text({ type: "text/html" }));
-
-// app.get("/", async (req, res) => {
-//   const { rows } = await pool.query("SELECT NOW()");
-//   res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
